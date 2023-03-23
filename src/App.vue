f0a7a62d830b98c0fff00fd14c14f5aef40d745a
Sambert Rodrigues <sambert@smartshore.nl>
2023-03-23 17:15:45 +0530

    reverted

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,22 @@
 // import { storeToRefs } from 'pinia';
 import { RouterView } from 'vue-router'
 import { useAuthStore } from './stores/auth/authStore';
-import Modal from './components/UI/TheModal.vue'
-import { ref } from 'vue';
 // const { user } = storeToRefs( useAuthStore )
 
 export default {
-  components:{ 
-    Modal 
-  },
   setup() {
-    const isModalVisible = ref(false)
     const authStore = useAuthStore();
     const userState = authStore.user
       console.log(userState)
     const handleLogout = () => {
       authStore.logout();
     }
-    
+
+
     return {
       // users,
       authStore,
       userState,
-      showModal,
-      closeModal,
       handleLogout
     }
   }
@@ -33,39 +26,13 @@ export default {
 </script>
 
 <template>
-  
   <main>
-    <button
-      type="button"
-      class="btn"
-      @click="showModal"
-    >
-      Open Modal!
-    </button>
-
-  <Modal
-    v-show="isModalVisible"
-    @close="closeModal"
-  >
-    <template v-slot:header>
-      This is a new modal header.
-    </template>
-
-    <template v-slot:body>
-      This is a new modal body.
-    </template>
-
-    <template v-slot:footer>
-      This is a new modal footer.
-    </template>
-  </Modal>
     <header>
       <img src="@/assets/logo.svg" alt="">
-      <h1>Pinia Tasks{{ isModalVisible }}</h1>
+      <h1>Pinia Tasks</h1>
       <button @click="authStore.refreshToken()" >refresh</button>
-      <button @click="handleLogout" v-show="userState">Logout</button>
+      <button @click="handleLogout" >Logout</button>
     </header>
     <RouterView />
   </main>
-  
 </template>
